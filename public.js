f121bb26f8020bb46c0d511a7221f6060d6d5813
kathydingcn <16864025@qq.com>
2017-08-30 09:50:57 +1000

    add toArray() in public

---
+++ public.js
@@ -0,0 +1,16 @@
+var public=(function(){
+
+    function toArray(likeArray){
+
+        var arr = Array.prototype.slice.call(likeArray);
+        console.log("to array====");
+        return arr;
+    }
+
+    return {toArray, toArray}
+
+
+})();
+
+
+
